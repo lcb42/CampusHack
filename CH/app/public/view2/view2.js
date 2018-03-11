94b33b1aceaf455a85ec18b7de92006fe152c8b2
lcb42 <blatherwick.lucy@gmail.com>
2018-03-11 02:19:32 +0000

    Gave up on 'See More' popup/popout

--- CH/app/public/view2/view2.js
+++ CH/app/public/view2/view2.js
@@ -27,31 +27,7 @@ angular.module('myApp.view2', ['ngRoute'])
 
     $scope.seemore = function (seemore) {
         console.log("blobworkingblob");
-        // Just provide a template url, a controller and call 'showModal'.
-        ModalService.showModal({
-            templateUrl: "yesno/yesno.html",
-            controller: "YesNoController"
-        }).then(function(modal) {
-            // The modal object has the element built, if this is a bootstrap modal
-            // you can call 'modal' to show it, if it's a custom modal just show or hide
-            // it as you need to.
-            modal.element.modal();
-            modal.close.then(function(result) {
-                $scope.message = result ? "You said Yes" : "You said No";
-            });
-        });
-
     }
-}]);
-
-app.controller('SampleController', ["$scope", "ModalService", function($scope, ModalService) {
-
-    $scope.showAModal = function(seemore) {
-
-
-
-    };
-
 }]);
 
 angular
